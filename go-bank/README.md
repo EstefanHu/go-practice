5254807f2d62562cb30fc872e10cf3c30962838e
estefan <estefanhu074@gmail.com>
2023-09-20 13:42:52 -0700

    batman

---
+++ go-bank/README.md
@@ -0,0 +1,1 @@
+Thanks to Anthony GG as this was a code along to his video
--- go-bank/api.go
+++ go-bank/api.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+    "net/http"
+    "github.com/gorilla/mux"
+
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+    w.WriteHeader(status)
+    w.Header().Set("Content-Type", "application/json")
+    return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+    Error string
+}
+
+func makeHTTPHanldeFunc(f apiFunc) http.HanlderFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if err := f(w, r); err != nil {
+            WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+        }
+    }
+}
+
 type APIServer struct {
     listenAddr string
 }
@@ -9,6 +35,12 @@ func NewAPIServer(listenAddr string) *APIServer {
     return &APIServer {
         listenAddr: listenAddr,
     }
+}
+
+func (s *APIServer) Run() {
+    router := mux.NewRouter()
+
+    router.handleFunc("/account", s.handleAccount)
 }
 
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
