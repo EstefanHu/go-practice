c71fd8850beb7c3bbaec45d81a9a0eb72844e32a
Estefan Hu <estefanhu074@gmail.com>
2024-08-08 23:04:48 -0700

    Aug 8th, 2024

---
+++ Tiago/gRPC/services/orders/grpc.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+type gRPCServer struct {
+	addr string
+}
+
+func NewGRPCServer(addr string) *gRPCServer {
+	return &gRPCServer{addr: addr}
+}
+
+func (s *NewGRPCServer) Run() error {
+	lis, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	// register our grpc services
+
+	log.Println("Starting gRPC server on", s.addr)
+
+	return grpcServer.Serve(lis)
+}
